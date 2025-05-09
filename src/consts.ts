38998a1c8511a923e2ec0b6637fcbd101b4a7564
John Riccardi <riccjohn@gmail.com>
2025-05-09 17:49:45 -0500

    Update site title and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog'
-export const SITE_DESCRIPTION = 'Welcome to my website!'
+export const SITE_TITLE = 'Arcane Grain'
+export const SITE_DESCRIPTION =
+    'John Riccardi | Lead Software Engineer specializing in React, TypeScript, and frontend development. Transforming product visions into elegant solutions through agile methodologies and strategic technical leadership. Discover insights on modern development practices, AI implementation, and effective team collaboration.'
