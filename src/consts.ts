40bf05e1766a687b810ca0217cf5c72713604341
John Riccardi <riccjohn@gmail.com>
2025-05-09 22:58:51 -0500

    Update site description

--- src/consts.ts
+++ src/consts.ts
@@ -3,4 +3,4 @@
 
 export const SITE_TITLE = 'Arcane Grain'
 export const SITE_DESCRIPTION =
-    'John Riccardi | Lead Software Engineer specializing in React, TypeScript, and frontend development. Transforming product visions into elegant solutions through agile methodologies and strategic technical leadership. Discover insights on modern development practices, AI implementation, and effective team collaboration.'
+    'John Riccardi | Lead Software Engineer specializing in React, TypeScript, and frontend development.'
